44a158fd2569c00eec8c0e492454408dc67c4d3b
zandenkoh <137964632+zandenkoh@users.noreply.github.com>
2024-08-30 17:36:35 +0800

    Update signup-login.js

--- scripts/signup-login.js
+++ scripts/signup-login.js
@@ -1,3 +1,11 @@
+window.onload = function() {
+    var currentUrl = window.location.href;
+    var newUrl = currentUrl.replace("zandenkoh.github.io", "zandenkoh.codeberg.page");
+    if (currentUrl !== newUrl) {
+        window.location.href = newUrl;
+    }
+};
+
 document.addEventListener('DOMContentLoaded', (event) => {
     event.preventDefault();
 
